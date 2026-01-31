3d77ca0f1144ea661194b9eeaebb9caa0e8799e2
Yeji Song <22100396@handong.ac.kr>
2026-01-31 13:48:29 +0500

    update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,13 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!
 
+
+console.log("Supabase URL:", supabaseUrl);  // 여기서 확인
+console.log("Supabase Key:", supabaseAnonKey);
+
+
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
 )
+
